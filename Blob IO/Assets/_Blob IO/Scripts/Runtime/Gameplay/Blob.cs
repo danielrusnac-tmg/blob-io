62dfe5b88e41d5c164db37b545665af2a805672f
Daniel <daniel.rusnac@trymygames.com>
2022-04-14 16:42:48 +0300

    Create simple springs

--- Blob IO/Assets/_Blob IO/Scripts/Runtime/Gameplay/Blob.cs
+++ Blob IO/Assets/_Blob IO/Scripts/Runtime/Gameplay/Blob.cs
@@ -2,12 +2,24 @@ using UnityEngine;
 
 namespace BlobIO.Gameplay
 {
-    public class Tentacle
-    {
-        
-    }
-
     public class Blob : MonoBehaviour
     {
+        [SerializeField] private float _distance = 2f;
+        [SerializeField] private Spring _spring;
+        [SerializeField] private Rigidbody2D _target;
+
+        private Rigidbody2D _rigidbody2D;
+
+        private void Awake()
+        {
+            _rigidbody2D = GetComponent<Rigidbody2D>();
+        }
+
+        private void FixedUpdate()
+        {
+            Vector2 offset = (_rigidbody2D.position - _target.position).normalized * _distance;
+            Vector3 force = _spring.CalculateForce(_target.position + offset, _rigidbody2D.position);
+            _rigidbody2D.AddForce(force * _rigidbody2D.mass);
+        }
     }
 }
---
+++ Blob IO/Assets/_Blob IO/Scripts/Runtime/Gameplay/Spring.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using UnityEngine;
+
+namespace BlobIO.Gameplay
+{
+    [Serializable]
+    public class Spring
+    {
+        [SerializeField] private float _length;
+        [SerializeField] private float _stiffness;
+        [SerializeField] private float _damp;
+
+        private Vector2 _lastOffset;
+        private Vector2 _velocity;
+
+        public Spring()
+        {
+            _length = 2f;
+            _stiffness = 500f;
+            _damp = 16f;
+        }
+
+        public Spring(float length, float stiffness, float damp)
+        {
+            _length = length;
+            _stiffness = stiffness;
+            _damp = damp;
+        }
+
+        public Vector2 CalculateForce(Vector2 staticPoint, Vector2 dynamicPoint)
+        {
+            Vector2 offset = dynamicPoint - staticPoint;
+            _velocity = (offset - _lastOffset) / Time.fixedDeltaTime;
+            _lastOffset = offset;
+            
+            Vector2 force = offset.magnitude / _length * -_stiffness * offset.normalized;
+            force -= _damp * _velocity;
+
+            return force;
+        }
+    }
+}
