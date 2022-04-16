68380382123de869f00357478df9752c1f45d469
Daniel Rusnac <daniel.rusnac@trymygames.com>
2022-04-16 06:15:45 +0300

    Updated level

---
+++ Blob IO/Assets/_Blob IO/Scripts/Editor/LevelEditor/LevelEditorStage.cs
@@ -0,0 +1,13 @@
+﻿using UnityEditor.SceneManagement;
+using UnityEngine;
+
+namespace BlobIO.LevelEditor
+{
+    public class LevelEditorStage : PreviewSceneStage
+    {
+        protected override GUIContent CreateHeaderContent()
+        {
+            return new GUIContent("Level Editor");
+        }
+    }
+}
