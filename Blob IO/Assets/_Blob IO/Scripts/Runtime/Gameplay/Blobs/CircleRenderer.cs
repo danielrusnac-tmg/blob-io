aba36f5cf842fdfeaf993e11ed325c9c891787f2
Daniel Rusnac <daniel.rusnac@trymygames.com>
2022-04-17 22:07:49 +0300

    Add custom circle mesh generation

---
+++ Blob IO/Assets/_Blob IO/Scripts/Runtime/Gameplay/Blobs/CircleRenderer.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+
+namespace BlobIO.Blobs
+{
+    public class CircleRenderer
+    {
+        private readonly int _count;
+        private readonly Mesh _mesh;
+        private Vector3 _center;
+        private readonly Vector3[] _vertices;
+        private readonly Vector3[] _normals;
+        private readonly int[] _triangles;
+
+        public CircleRenderer(MeshFilter meshFilter, int count)
+        {
+            _count = count;
+            _mesh = new Mesh();
+            meshFilter.mesh = _mesh;
+            
+            _vertices = new Vector3[count + 1];
+            _normals = new Vector3[count + 1];
+            _triangles = new int[count * 3];
+        }
+
+        public void UpdateMesh(Vector3[] points)
+        {
+            CalculateMeshTriangulation(points);
+            CalculateNormals(points);
+            UpdateMeshData();
+        }
+
+        private void CalculateNormals(Vector3[] points)
+        {
+            for (int i = 0; i < _count; i++)
+                _normals[i] = (points[i] - _center).normalized;
+            
+            _normals[_count] = -Vector3.forward;
+        }
+
+        private void UpdateMeshData()
+        {
+            _mesh.SetVertices(_vertices);
+            _mesh.SetTriangles(_triangles, 0);
+            _mesh.SetNormals(_normals);
+            _mesh.RecalculateBounds();
+        }
+
+        private void CalculateMeshTriangulation(Vector3[] points)
+        {
+            _center = Vector3.zero;
+            
+            for (int i = 0, triangle = 0; i < _count; i++, triangle += 3)
+            {
+                _center += points[i];
+                _vertices[i] = points[i];
+                CreateTriangle(triangle, i);
+            }
+
+            _center /= _count;
+            _vertices[_count] = _center;
+        }
+
+        private void CreateTriangle(int triangle, int vertex)
+        {
+            if (vertex < _count - 1)
+            {
+                _triangles[triangle + 0] = vertex + 1;
+                _triangles[triangle + 1] = vertex;
+                _triangles[triangle + 2] = _count;
+            }
+            else
+            {
+                _triangles[triangle + 0] = 0;
+                _triangles[triangle + 1] = _count - 1;
+                _triangles[triangle + 2] = _count;
+            }
+        }
+    }
+}
---
+++ Blob IO/Assets/_Blob IO/Scripts/Runtime/Gameplay/Blobs/DeformableBlob.cs
@@ -0,0 +1,47 @@
+﻿using BlobIO.Controllers;
+using UnityEngine;
+
+namespace BlobIO.Blobs
+{
+    public class DeformableBlob : MonoBehaviour, IControllable
+    {
+        [SerializeField] private int _pointCount = 32;
+        [SerializeField] private float _radius = 0.5f;
+        [SerializeField] private MeshFilter _meshFilter;
+
+        private CircleRenderer _circleRenderer;
+        private IControllableInput _input;
+
+        private void Awake()
+        {
+            _circleRenderer = new CircleRenderer(_meshFilter, _pointCount);
+        }
+
+        private void Update()
+        {
+            UpdateCircleMesh();
+        }
+
+        public void SetInput(IControllableInput input)
+        {
+            _input = input;
+        }
+
+        private void UpdateCircleMesh()
+        {
+            _circleRenderer.UpdateMesh(GenerateCircleVertices(_pointCount, _radius, Vector3.zero));
+        }
+
+        private Vector3[] GenerateCircleVertices(int pointCount, float radius, Vector3 center)
+        {
+            Vector3[] result = new Vector3[pointCount];
+
+            float angleStep = 360f / pointCount;
+
+            for (int i = 0; i < pointCount; i++)
+                result[i] = center + Quaternion.AngleAxis(angleStep * i, Vector3.forward) * Vector3.right * radius;
+            
+            return result;
+        }
+    }
+}
