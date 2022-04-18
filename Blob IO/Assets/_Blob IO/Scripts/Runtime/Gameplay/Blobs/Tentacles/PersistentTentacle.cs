c95f35d69690d81a93a5e548072fe604da3dd268
Daniel <daniel.rusnac@trymygames.com>
2022-04-18 11:31:58 +0300

    Working on persistent tentacle

---
+++ Blob IO/Assets/_Blob IO/Scripts/Runtime/Gameplay/Blobs/Tentacles/PersistentTentacle.cs
@@ -0,0 +1,85 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace BlobIO.Blobs.Tentacles
+{
+    public class PersistentTentacle : MonoBehaviour
+    {
+        private static readonly RaycastHit2D[] s_tentacleHits;
+        
+        [SerializeField] private PersistentTentacleSetting _setting;
+
+        private bool _isGrabbing;
+        private TentaclePoint _basePoint;
+        private TentaclePoint _grabPoint;
+        
+        private Vector2 GetTentacleDirection => transform.up;
+        private Vector2 GetTentacleOrigin => transform.position;
+        private Vector2 GetTentacleTip => transform.position + transform.up * _setting.Radius;
+
+        static PersistentTentacle()
+        {
+            s_tentacleHits = new RaycastHit2D[1];
+        }
+
+        private void Awake()
+        {
+            _basePoint = new TentaclePoint(gameObject, GetTentacleOrigin);
+        }
+
+        private void Update()
+        {
+            if (_isGrabbing)
+            {
+                Vector2 offset = _grabPoint.Position - GetTentacleOrigin;
+                
+                if (Vector2.Distance(GetTentacleOrigin, _grabPoint.Position) > _setting.ReleaseDistance ||
+                    Physics2D.RaycastNonAlloc(GetTentacleOrigin, offset.normalized, s_tentacleHits, offset.magnitude - 0.1f,
+                        _setting.WallMask) > 0)
+                {
+                    Release();
+                }
+            }
+            
+            if (Physics2D.RaycastNonAlloc(GetTentacleOrigin, GetTentacleDirection, s_tentacleHits, _setting.Radius, _setting.WallMask) > 0)
+            {
+                if (!_isGrabbing || Vector2.Distance(s_tentacleHits[0].point, _grabPoint.Position) > _setting.StepDistance)
+                    Grab(s_tentacleHits[0].point, s_tentacleHits[0].collider.gameObject);
+            }
+        }
+
+        private void OnDrawGizmosSelected()
+        {
+#if UNITY_EDITOR
+            if (_setting == null)
+                return;
+
+            if (!_isGrabbing)
+            {
+                Handles.DrawLine(GetTentacleOrigin, GetTentacleTip);
+            }
+            else 
+            {
+                if (_grabPoint != null)
+                {
+                    float compression = Vector2.Distance(GetTentacleOrigin, _grabPoint.Position) / _setting.Radius;
+                    Handles.color = Color.Lerp(Color.green, Color.red, compression);
+                    Handles.DrawLine(GetTentacleOrigin, _grabPoint.Position);
+                    Handles.DrawSolidDisc(_grabPoint.Position, Vector3.forward, 0.1f);
+                }
+            }
+#endif
+        }
+
+        private void Grab(Vector2 point, GameObject target)
+        {
+            _grabPoint = new TentaclePoint(target, point);
+            _isGrabbing = true;
+        }
+
+        private void Release()
+        {
+            _isGrabbing = false;
+        }
+    }
+}
---
+++ Blob IO/Assets/_Blob IO/Scripts/Runtime/Gameplay/Blobs/Tentacles/PersistentTentacleSetting.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace BlobIO.Blobs.Tentacles
+{
+    [CreateAssetMenu(fileName = "New Persistent Tentacle Setting", menuName = CreationPaths.CREATE + "Persistent Tentacle Setting")]
+    public class PersistentTentacleSetting : ScriptableObject
+    {
+        [SerializeField] private LayerMask _wallMask;
+        [SerializeField] private float _radius = 8f;
+        [SerializeField] private float _stepDistance = 3f;
+        [SerializeField] private float _releaseDistance = 8f;
+
+        public LayerMask WallMask => _wallMask;
+        public float Radius => _radius;
+        public float StepDistance => _stepDistance;
+        public float ReleaseDistance => _releaseDistance;
+    }
+}
