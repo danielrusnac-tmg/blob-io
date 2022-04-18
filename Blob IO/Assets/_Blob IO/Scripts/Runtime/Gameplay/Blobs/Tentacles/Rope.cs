e8746c73783f2de1625ab4d08914828c6bbd4f4d
Daniel Rusnac <daniel.rusnac@trymygames.com>
2022-04-19 00:10:15 +0300

    Create simple rope & Update materials

---
+++ Blob IO/Assets/_Blob IO/Scripts/Runtime/Gameplay/Blobs/Tentacles/Rope.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using UnityEngine;
+
+namespace BlobIO.Blobs.Tentacles
+{
+    public class Rope : MonoBehaviour
+    {
+        [SerializeField] private float _length = 1f;
+        [SerializeField] private float _resolution = 30f;
+        [SerializeField] private int _constrainSteps = 50;
+        [SerializeField] private Vector3 _gravity;
+        [SerializeField] private LineRenderer _lineRenderer;
+
+        private int _pointCount;
+        private float _pointDistance;
+        private Vector3[] _points;
+
+        private void Awake()
+        {
+            CreatePoints();
+        }
+
+        private void Update()
+        {
+            ApplyGravity();
+
+            for (var i = 0; i < _constrainSteps; i++)
+                ApplyConstrains();
+
+            _lineRenderer.SetPositions(_points);
+        }
+
+        private void OnDrawGizmosSelected()
+        {
+            Gizmos.DrawRay(transform.position, -transform.up * _length);
+        }
+
+        private void CreatePoints()
+        {
+            _pointCount = (int) (_length / (1f / _resolution));
+            _pointDistance = _length / _pointCount;
+            _points = new Vector3[_pointCount];
+            _lineRenderer.positionCount = _pointCount;
+        }
+
+        private void ApplyConstrains()
+        {
+            for (int i = 0; i < _pointCount - 1; i++)
+            {
+                float distance = (_points[i] - _points[i + 1]).magnitude;
+                float error = distance - _pointDistance;
+                Vector3 changeDir = (_points[i] - _points[i + 1]).normalized;
+                Vector3 changeAmount = changeDir * error;
+
+                if (i != 0)
+                {
+                    _points[i] -= changeAmount * 0.5f;
+                    _points[i + 1] += changeAmount * 0.5f;
+                }
+                else
+                {
+                    _points[i + 1] += changeAmount;
+                }
+            }
+        }
+
+        private void ApplyGravity()
+        {
+            for (int i = 0; i < _pointCount; i++)
+            {
+                _points[i] += _gravity * Time.deltaTime;
+            }
+
+            _points[0] = transform.position;
+        }
+    }
+}
--- Blob IO/Assets/_Blob IO/Scripts/Runtime/Gameplay/Blobs/Tentacles/Tentacle.cs
+++ Blob IO/Assets/_Blob IO/Scripts/Runtime/Gameplay/Blobs/Tentacles/Tentacle.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using UnityEditor;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace BlobIO.Blobs.Tentacles
 {
