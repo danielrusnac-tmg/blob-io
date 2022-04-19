55bc9e0341b3c9ca12cf2bec128653fd88b10c1d
Daniel <daniel.rusnac@trymygames.com>
2022-04-19 10:24:13 +0300

    Return to spring blob

---
+++ Blob IO/Assets/_Blob IO/Scripts/Runtime/Gameplay/Blobs/BetterBlob.cs
@@ -0,0 +1,17 @@
+﻿using BlobIO.Controllers;
+using UnityEngine;
+
+namespace BlobIO.Blobs
+{
+    public class BetterBlob : MonoBehaviour, IControllable
+    {
+        [SerializeField] private int _tentacleCount = 10;
+
+        private IControllableInput _input;
+
+        public void SetInput(IControllableInput input)
+        {
+            _input = input;
+        }
+    }
+}
--- Blob IO/Assets/_Blob IO/Scripts/Runtime/Services/Factory/GameFactory.cs
+++ Blob IO/Assets/_Blob IO/Scripts/Runtime/Services/Factory/GameFactory.cs
@@ -21,7 +21,7 @@ namespace BlobIO.Services.Factory
         
         public async void CreatePlayer(Vector3 position)
         {
-            GameObject playerPrefab = await _assetProvider.Load(AssetPaths.WEIGHTED_BLOB);
+            GameObject playerPrefab = await _assetProvider.Load(AssetPaths.SPRING_BLOB);
             _player = Object.Instantiate(playerPrefab, position, Quaternion.identity);
 
             SetPlayerInput();
